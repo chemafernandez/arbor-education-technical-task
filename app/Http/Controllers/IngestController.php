bf5337870c3d9d77209ddf5b3cf4bc27683fe9f1
Chema Fernández <chema@Chema2021.local>
2025-06-19 14:41:01 +0100

    Improve DI

--- app/Http/Controllers/IngestController.php
+++ app/Http/Controllers/IngestController.php
@@ -26,6 +26,23 @@ class IngestController extends Controller
     private FindableOrCreateableInterface $messageFindable;
     private FindableInterface $statusFindable;
 
+    public function __construct(
+        RecipientFindableOrCreateable $recipientFindable,
+        SenderFindableOrCreateable $senderFindable,
+        StudentFindableOrCreateable $studentFindable,
+        ProviderFindableOrCreateable $providerFindable,
+        MessageFindableOrCreateable $messageFindable,
+        StatusFindable $statusFindable,
+    )
+    {
+        $this->recipientFindable = $recipientFindable;
+        $this->senderFindable = $senderFindable;
+        $this->studentFindable = $studentFindable;
+        $this->providerFindable = $providerFindable;
+        $this->messageFindable = $messageFindable;
+        $this->statusFindable = $statusFindable;
+    }
+
     /**
      * Populate the database with data from a json file
      */
@@ -39,27 +56,21 @@ class IngestController extends Controller
 
             foreach($smsList as $sms) {
                 // Insert recipient if it doesn't exist
-                $this->recipientFindable = new RecipientFindableOrCreateable();
                 $sms->recipient_id = $this->recipientFindable->findOrCreate($sms);
 
                 // Insert sender if it doesn't exist
-                $this->senderFindable = new SenderFindableOrCreateable();
                 $sms->sender_id = $this->senderFindable->findOrCreate($sms);
 
                 // Insert student if it doesn't exist
-                $this->studentFindable = new StudentFindableOrCreateable();
                 $sms->student_id = $this->studentFindable->findOrCreate($sms);
 
                 // Insert provider if it doesn't exist
-                $this->providerFindable = new ProviderFindableOrCreateable();
                 $sms->provider_id = $this->providerFindable->findOrCreate($sms);
 
                 // Find status
-                $this->statusFindable = new StatusFindable();
                 $sms->status_id = $this->statusFindable->find($sms);
 
                 // Insert message if it doesn't exist
-                $this->messageFindable = new MessageFindableOrCreateable();
                 $sms->message_id = $this->messageFindable->findOrCreate($sms);
             }
 
